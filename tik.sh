496d3cbc7859292ade71e4c2524decf5caad9d2d
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-24 02:14:56 +0300

    xx
    xx

--- tik.sh
+++ tik.sh
@@ -1,4 +1,9 @@
 cd /content/drive/MyDrive/tt/
+pip install TikTokDownload
+pip install requests
+pip install retrying
+pip install flask
+pip install flask-cors
 chmod +x build.bat
 ./build.bat
 python TikTokMulti.py
