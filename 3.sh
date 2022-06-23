7dae50231ec52210ef331e80407b575ae1308252
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-23 17:10:29 +0300

    xx
    xx

---
+++ 3.sh
@@ -0,0 +1,50 @@
+ffmpeg -i 1.mp4 -f srt -i 1__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 1F.mp4
+
+ffmpeg -i 2.mp4 -f srt -i 2__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 2F.mp4
+
+ffmpeg -i 3.mp4 -f srt -i 3__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 3F.mp4
+
+ffmpeg -i 4.mp4 -f srt -i 4__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 4F.mp4
+
+ffmpeg -i 5.mp4 -f srt -i 5__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 5F.mp4
+
+ffmpeg -i 6.mp4 -f srt -i 6__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 6F.mp4
+
+ffmpeg -i 7.mp4 -f srt -i 7__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 7F.mp4
+
+ffmpeg -i 8.mp4 -f srt -i 8__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 8F.mp4
+
+ffmpeg -i 9.mp4 -f srt -i 9__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 9F.mp4
+
+ffmpeg -i 10.mp4 -f srt -i 10__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 10F.mp4
+
+ffmpeg -i 11.mp4 -f srt -i 11__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 11F.mp4
+
+ffmpeg -i 12.mp4 -f srt -i 12__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 12F.mp4
+ffmpeg -i 13.mp4 -f srt -i 13__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 13F.mp4
+ffmpeg -i 14.mp4 -f srt -i 14__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 14F.mp4
+ffmpeg -i 15.mp4 -f srt -i 15__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 51F.mp4
+ffmpeg -i 16.mp4 -f srt -i 16__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 61F.mp4
+ffmpeg -i 17.mp4 -f srt -i 17__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 17F.mp4
+ffmpeg -i 18.mp4 -f srt -i 18__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 18F.mp4
+ffmpeg -i 19.mp4 -f srt -i 19__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 19F.mp4
+
+ffmpeg -i 20.mp4 -f srt -i 20__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 20F.mp4
+ffmpeg -i 21.mp4 -f srt -i 21__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 21F.mp4
+ffmpeg -i 22.mp4 -f srt -i 22__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 22F.mp4
+ffmpeg -i 23.mp4 -f srt -i 23__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 23F.mp4
+ffmpeg -i 24.mp4 -f srt -i 24__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 24F.mp4
+ffmpeg -i 25.mp4 -f srt -i 25__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 25F.mp4
+ffmpeg -i 26.mp4 -f srt -i 26__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 26F.mp4
+ffmpeg -i 27.mp4 -f srt -i 27__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 27F.mp4
+ffmpeg -i 28.mp4 -f srt -i 28__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 28F.mp4
+ffmpeg -i 29.mp4 -f srt -i 29__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 29F.mp4
+
+ffmpeg -i 30.mp4 -f srt -i 30__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 30F.mp4
+ffmpeg -i 31.mp4 -f srt -i 31__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 31F.mp4
+ffmpeg -i 32.mp4 -f srt -i 32__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 32F.mp4
+ffmpeg -i 33.mp4 -f srt -i 33__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 33F.mp4
+ffmpeg -i 34.mp4 -f srt -i 34__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 34F.mp4
+ffmpeg -i 35.mp4 -f srt -i 35__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 35F.mp4
+ffmpeg -i 36.mp4 -f srt -i 36__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 36F.mp4
+ffmpeg -i 37.mp4 -f srt -i 37__ar-SA.srt -c:v copy -c:a copy -c:s mov_text 37F.mp4
