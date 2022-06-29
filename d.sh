4d59f049bdc82af68448c910a12b992a5721d593
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-29 21:25:45 +0300

    xx
    xxx

--- d.sh
+++ d.sh
@@ -1,2 +1,10 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb  > /dev/null 2>&1
 sudo apt install ./google-chrome-stable_current_amd64.deb 
+sudo apt install vlc -y 
+sudo apt install firefox -y 
+curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/anydesk.gpg > /dev/null 2>&1
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install anydesk > /dev/null 2>&1
+sudo apt install ffmpeg -y
+sudo apt install kdenlive
