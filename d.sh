d4a135ac25d1d88fef0a091f9a1ae9e88557af87
Omaromar2255 <97483395+Omaromar2255@users.noreply.github.com>
2022-06-29 22:49:13 +0300

    xx
    xx

--- d.sh
+++ d.sh
@@ -5,6 +5,11 @@ sudo apt install firefox -y
 curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/anydesk.gpg > /dev/null 2>&1
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null 2>&1
 sudo apt update > /dev/null 2>&1
-sudo apt install anydesk > /dev/null 2>&1
+sudo apt install anydesk 
 sudo apt install ffmpeg -y
 sudo apt install kdenlive
+sudo apt-get install libavcodec-extra-53
+sudo apt-get install -y xarchiver
+sudo apt-get install handbrake
+sudo apt install kdenlive
+
